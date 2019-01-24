7871da52f9ed20cf729ea1e70c8b6ae21363b9f7
Felix <f.samplexyt@gmail.com>
2019-01-24 11:30:04 +0100

    Updates

---
+++ precache-manifest.6f640e258ebe6b0ae55b8140c22a98d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fe881a7068793e8486e",
+    "url": "/my-app/static/css/main.62a3ddc1.chunk.css"
+  },
+  {
+    "revision": "5fe881a7068793e8486e",
+    "url": "/my-app/static/js/main.5fe881a7.chunk.js"
+  },
+  {
+    "revision": "6da987ce309a1616b66f",
+    "url": "/my-app/static/css/1.6291976f.chunk.css"
+  },
+  {
+    "revision": "6da987ce309a1616b66f",
+    "url": "/my-app/static/js/1.6da987ce.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "a8d8621aa26c8802398fa59a6bbb9028",
+    "url": "/my-app/index.html"
+  }
+];
